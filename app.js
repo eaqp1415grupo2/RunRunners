3a02d4e9943e77af30873617c8d6041528413127
draja <raja-27@hotmail.com>
2015-04-21 17:12:06 +0200

    Subiendo Proyecto

--- app.js
+++ app.js
@@ -8,25 +8,28 @@ app.configure(function (){
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+    app.engine("jade", require("jade").__express);
+    app.set("views", "./views");
+    app.set("view engine", "jade");
 
 });
-
+routes = require('./routes/api')(app);
 
 app.get('/', function(req, res){
-
-		res.send("Hello World!");
+    res.render("index");
+		//res.send("Hello World!");
 });
 
 //routes = require('./routes/users')(app);
 
-//mongoose.connect('mongodb://localhost/usuarios', function(err, res){
-//	if(err){
-//		console.log('ERROR: connecting to database.' + err);
-//	}
-//	else{
-//		console.log('Connected to Database');
-//	}
-//});
+mongoose.connect('mongodb://localhost/usuarios', function(err, res){
+	if(err){
+		console.log('ERROR: connecting to database.' + err);
+	}
+	else{
+		console.log('Connected to Database');
+	}
+});
 
 //servidor escuchando en el puerto 5000
 app.listen(5000)
---
+++ routes/api.js
@@ -0,0 +1,14 @@
+/**
+ * Created by david on 14/04/2015.
+ */
+module.exports = function(app) {
+
+
+
+
+
+
+
+
+
+}

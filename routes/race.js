0321769c1c58278ca21962eaf4db91c14f06a3c8
ajimenezherrero <ajimenezherrero@gmail.com>
2015-04-28 20:30:50 +0200

    Funciones de update añadidas a Race

--- routes/race.js
+++ routes/race.js
@@ -13,8 +13,8 @@ module.exports = function(app) {
     };
 
     //GET - Return all races in the DB by ID_Race
-    findByIdRace = function(req, res) {
-        Race.findById(req.param.id, function(err, race) {
+    findRaceByID = function(req, res) {
+        Race.findById({"ID_Race":req.param.ID_Race}, function(err, race) {
             if(!err) {
                 res.send(race);
             } else {
@@ -54,7 +54,7 @@ module.exports = function(app) {
 
     //PUT - Update a register already exists
     updateRace = function(req, res) {
-        Race.findById(req.params.id, function(err, race) {
+        Race.findById({"ID_Race":req.param.ID_Race}, function(err, race) {
             race.ID_Race = req.body.ID_Race;
             race.Name = req.body.Name;
             race.Level = req.body.Level;
@@ -79,7 +79,7 @@ module.exports = function(app) {
 
     //DELETE - Delete a TVShow with specified ID
     deleteRace = function(req, res) {
-        Race.findById(req.params.id, function(err, race) {
+        Race.findById({"ID_Race":req.param.ID_Race}, function(err, race) {
             race.remove(function(err) {
                 if(!err) {
                     console.log('Removed');
@@ -90,11 +90,43 @@ module.exports = function(app) {
         });
     }
 
+    addUser = function (req, res) {
+        Race.findById({"ID_Race":req.param.ID_Race}, function(err, user) {
+            if (Race.findOne({"Users.Username": req.body.Username}) == null) {
+                if (req.body.Username != null) user.Users.push(req.body);
+                else console.log("Something wrong with: " + req.body);
+                user.save(function (err) {
+                    if (err) console.log("Error: " + err);
+                    else console.log("User Inserted in Group");
+                });
+                res.send(user);
+            }
+            else console.log("This User is inside this race already");
+        });
+
+    };
+
+    addMessages = function (req, res) {
+        Race.findById({"ID_Race":req.param.ID_Race}, function(err, message) {
+            if (message.body.Username != null && message.body.Text != null)  message.Messages.push(req.body);
+            else console.log("Something wrong with: " + req.body);
+            message.save(function (err) {
+                if (err) console.log("Error: " + err);
+                else console.log("User Inserted in Group");
+            });
+            res.send(message);
+        });
+
+    };
+
+
     //Link routes and functions
     app.get('/race', findAllRaces);
-    app.get('/race/:id', findByIdRace);
+    app.get('/race/:id_race', findRaceByID);
     app.post('/race', addRace);
     app.put('/race/:id', updateRace);
     app.delete('/race/:id', deleteRace);
+    app.put('/race/:id', addUser);
+    app.put('/race/:id', addMessages);
 
 }

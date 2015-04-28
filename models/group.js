7f763c4c7131291d7f167db48ec56e97e7c9c185
ferrandiaz <fdia13350@gmail.com>
2015-04-28 10:52:42 +0200

    Group model

---
+++ models/group.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var groupSchema = new Schema({
+    ID_Group: {type: String},
+    Name: {type: String},
+    Info: {type: String},
+    Level: {
+        type: String, enum: ['Beginner', 'Initiated', 'Professional']
+    },
+    LocationIni: {
+        Lng: {type: Number},
+        Ltd: {type: Number}
+    },
+    Admin_Group: {type: String},
+    Users: {
+        Username: {type: String}
+    },
+    RacesDone: {
+        Race: {type: String}
+    },
+    RacesPending: {
+        Race: {type: String}
+    },
+    Messages: {
+        Username: {type: String},
+        Message: {type: String}
+    }
+
+});
+
+module.exports = mongoose.model('Group', groupSchema);

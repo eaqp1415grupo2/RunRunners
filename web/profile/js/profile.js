d4cceb65fffd06cc53986e14b54c2d05d251f4e4
draja <raja-27@hotmail.com>
2015-05-07 17:55:49 +0200

    Consultar Api

--- web/profile/js/profile.js
+++ web/profile/js/profile.js
@@ -1,3 +1,45 @@
 /**
  * Created by david on 4/5/15.
  */
+$(document).ready(function() {
+    Userdata();
+});
+
+function Userdata() {
+    var url = 'http://localhost:3000/users/David';
+
+    console.log(url);
+
+    $('#searchtab').text('');
+
+    $.ajax({
+        url : url,
+        type : 'GET',
+        crossDomain : true,
+        dataType : 'json'
+    }).done(function(data, status, jqxhr) {
+
+        var listausers = data;
+        console.log(listausers);
+
+
+
+
+
+        $('<a href="#"  style="color: #00FF00"><strong> Name: </strong></a>' + listausers.name + '<br>').appendTo($('#name'));
+        //$('<a href="#"  style="color: #00FF00"><strong> Email: </strong></a>' + listausers.email + '<br>').appendTo($('#emailofuser'));
+        //$('<a href="#"  style="color: #00FF00"><strong> Description: </strong></a>' + listausers.description + '<br>').appendTo($('#descriptionofuser'));
+
+
+
+
+
+
+
+
+
+    }).fail(function() {
+        $('<div class="alert alert-danger"> <strong>Oh!</strong> Todo not found </div>').appendTo($("#myResults"));
+    });
+
+}

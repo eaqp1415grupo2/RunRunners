4d6bae76d6766d9e0f0ed22e9370dc5c30edbf3c
AlbertSantamaria <santamaria.lara@gmail.com>
2015-05-14 21:56:02 +0200

    Implementado seguridad SSL para HTTP

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ app.configure(function () {
     app.use(app.router);
 });
 
+var https = require('https'),
+fs = require('fs');
+var sslOptions = {
+  key: fs.readFileSync('./ssl/server.key'),
+  cert: fs.readFileSync('./ssl/server.crt'),
+  ca: fs.readFileSync('./ssl/ca.crt'),
+  requestCert: true,
+  rejectUnauthorized: false
+};
+var secureServer = https.createServer(sslOptions,app).listen('3030', function(){
+  console.log("Secure Express server listening on port 3030");
+});
+
 
 //habilita los archivos secundarios de resources y js necesarios de /web
 app.use(express.static('web'));

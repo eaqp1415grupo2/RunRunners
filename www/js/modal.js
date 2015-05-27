e31c7d6131ea6e3be243f588e6e4637d1512a61f
david <david_bcn_210@hotmail.com>
2015-05-27 23:21:43 +0200

    perfil usuario con token
    perfil usuario con token

--- www/js/modal.js
+++ www/js/modal.js
@@ -1,9 +1,25 @@
+// window.onload = listartodo();
+//var app = angular.module("app", ['ui.bootstrap']);
+/*var profileapp = angular.module('profileapp', []);
+profileapp.controller('getctrl', function($scope, $http, $window) {
+    $scope.formData = {};
+    $scope.listartodo = function() {
+        $http.get('https://localhost:3030/user/alex')//+ $window.localStorage.token)
+            .success(function (data) {
+                $scope.users = data;
+                console.log(data);
+            })
+            .error(function (data) {
+                console.log('Error:' + data);
+            });
+    }
 
-angular.module('ui.bootstrap.demo', ['ui.bootstrap'])
+});*/
+//angular.module('ui.bootstrap.demo', ['ui.bootstrap'])
+angular.module('profileapp', ['ui.bootstrap'])
 
-
-    .controller("GetCtrl", function($scope, $http) {
-        $http.get('https://localhost:3030/user/username/David')
+    .controller("getctrl", function($scope, $http) {
+        $http.get('https://localhost:3030/user/'+ window.localStorage.token)
             .success(function (data) {
                 $scope.users = data;
                 console.log(data);
@@ -12,9 +28,9 @@ angular.module('ui.bootstrap.demo', ['ui.bootstrap'])
                 console.log('Error:' + data);
             });
 
-    })
+    });
 
-angular.module('ui.bootstrap.demo').controller('ModalDemoCtrl', function ($scope, $modal) {
+angular.module('profileapp').controller('ModalDemoCtrl', function ($scope, $modal) {
 
 
     $scope.openUpdate = function () {
@@ -37,11 +53,11 @@ angular.module('ui.bootstrap.demo').controller('ModalDemoCtrl', function ($scope
 // It is not the same as the $modal service used above.
 
 
-angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrlUpdate', function ($scope, $modalInstance, $http) {
+angular.module('profileapp').controller('ModalInstanceCtrlUpdate', function ($scope, $modalInstance, $http) {
 
 
     $scope.okUpdate= function () {
-        $http.put('user/David',  $scope.updateUser)//+ cookie o token)
+        $http.put('user/' + window.localStorage.token,  $scope.updateUser)//+ cookie o token)
             .success(function(data) {
                 // alert("acabas de actualizar el usuario")  //-cookie o token de usuario
                 window.location.href='/profile';
@@ -56,10 +72,10 @@ angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrlUpdate', functi
 
 
 });
-angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrlDelete', function ($scope, $modalInstance, $http) {
+angular.module('profileapp').controller('ModalInstanceCtrlDelete', function ($scope, $modalInstance, $http) {
 
     $scope.okDelete= function () {
-        $http.delete('user/David' )//+ cookie o token)
+        $http.delete('user/' + window.localStorage.token )//+ cookie o token)
             .success(function(data) {
                 alert("acabas de borrar el usuario, le redigiremos al inicio");  //-cookie o token de usuario
                 window.location.href='/';

1fa0dee8279d53600c916625560a149e516fd354
david <david_bcn_210@hotmail.com>
2015-06-11 13:04:46 +0200

    corrección

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -487,115 +487,199 @@ function RacesCtrl($scope, $http, $stateParams, $ionicLoading, $log) {
 }
 
 
-
 /**
  * A google map / GPS controller.
  */
 MapApp.controller('GpsCtrl', ['$scope','$http','$ionicPlatform', '$location',
-	function($scope,$http, $ionicPlatform) {
+	function($scope,$http, $route, $window,$ionicPlatform, $ionicLoading, $location) {
+
+			//$location.url('/map/home');
+	//	$ionicPlatform.ready(function() {
+		//$scope.$on({reload: true});
+		//$state($state.current, {}, {reload: true});
 		$scope.races = {};
-		var races;
+		var races, poly, map, markers;
 		// init gps array
 		$scope.whoiswhere = [];
 		$scope.basel = {lat: 41.3868765, lon: 2.1700207};
 		// geo-coding
 		$scope.search = "";
-				navigator.geolocation.watchPosition(function (position) {
-					//$scope.position=position;
-					var c = position.coords;
-					$scope.gotoLocation(c.latitude, c.longitude);
+		var infowindow;
+		var currentMarkers;
+		var callbackName = 'InitMapCb';
+		/* $scope.$on( "$ionicView.enter", function( scopes, states ) {
+			google.maps.event.trigger( map, 'resize' );
+		});*/
+		//$ionicPlatform.ready(function()  {
+			//$ionicLoading.ready(function() {
+		var directionsDisplay;
+		var directionsService = new google.maps.DirectionsService();
+		var inticor, start, end ;
+
+		function initialize() {
+			var mapOptions = {
+				zoom: 13,
+				center: new google.maps.LatLng(47.55, 7.59),
+				mapTypeId: google.maps.MapTypeId.ROADMAP,
+				panControl: true,
+				zoomControl: true,
+				mapTypeControl: true,
+				scaleControl: false,
+				refresh: true,
+				streetViewControl: true,
+				navigationControl: true,
+				disableDefaultUI: true,
+				overviewMapControl: true,
+				addressControlOptions: {
+					position: google.maps.ControlPosition.BOTTOM_CENTER
+				}
+
+			};
+
+
+		/*	directionsDisplay = new google.maps.DirectionsRenderer();
+			var inticor= new google.maps.LatLng("Your Lat Long here");
+			map = new google.maps.Map(document.getElementById('map-canvas'),
+				mapOptions);
+			directionsDisplay.setMap(map);
+			calcRoute();
+			*/
+			// Add a listener for the click event
+			google.maps.event.addListener(map, 'click', addLatLng);
+		}
+
+		google.maps.event.addDomListener(window, 'load', initialize);
+		// Add a listener for the click event+
+/*
+		function calcRoute() {
+			alert("calculando ruta...");
+			var start = (41.386608, 2.16402);
+			var end = (41.3886325,2.168484);
+			var request = {
+				origin:start,
+				destination:end,
+				travelMode: google.maps.TravelMode.WALKING
+			};
+			directionsService.route(request, function(response, status) {
+				if (status == google.maps.DirectionsStatus.OK) {
+					alert("in");
+					directionsDisplay = new google.maps.DirectionsRenderer();
+					directionsDisplay.setMap(map);
+					directionsDisplay.setDirections(response);
+				}
+			});
+		}
+		*/
+
+		navigator.geolocation.getCurrentPosition(function (position) {
+			//$scope.position=position;
+			var c = position.coords;
+			$scope.gotoLocation(c.latitude, c.longitude);
+			$scope.$apply();
+		}, function (e) {
+			console.log("Error retrieving position " + e.code + " " + e.message)
+		});
+		$scope.gotoLocation = function (lat, lon) {
+			if ($scope.lat != lat || $scope.lon != lon) {
+				$scope.basel = {lat: lat, lon: lon};
+				// to be user as markers, objects should have "lat", "lon", and "name" properties
+				$scope.whoiswhere = [ //marker
+					{
+						"name": "You are here!",
+						"lat": $scope.basel.lat,
+						"lon": $scope.basel.lon
+
+					}
+				];
+
+				if (!$scope.$$phase) $scope.$apply("basel");
+
+			}
+		};
+
+		$scope.centerOnMe = function() {
+
+			alert("centrando");
+
+		}
+
+
+		$http.get(URL + 'race', $scope).success(function (data) {
+
+				races = data;
+				angular.forEach(races, function (race) {
+					$scope.objects = race;
+					$scope.Name = race.Name;
+					$scope.Distance = race.Distance;
+					$scope.Level = race.Level;
+					$scope.Date = race.Date;
+					$scope.LocationIniLng = race.LocationIni.Lng;
+					$scope.LocationIniLtd = race.LocationIni.Ltd;
+					goto(race);
+				//	localizarte(position);
+					console.log($scope.LocationIniLng);
+					console.log($scope.LocationIniLtd);
+
+
+				});
+			});
+
+			function goto(race) {
+				navigator.geolocation.getCurrentPosition(function (position) {
+					//	for (var i = 0; i < 2; i++) {
+
+					var lat = race.LocationIni.Lng;
+					var lon = race.LocationIni.Ltd;
+					var name = race.Name;
+					gotolocation2(lat, lon, name);
 					$scope.$apply();
+					//}
 				}, function (e) {
 					console.log("Error retrieving position " + e.code + " " + e.message)
 				});
-
-
-				$scope.gotoLocation = function (lat, lon) {
+				function gotolocation2(lat, lon, name) {
+					//alert (lat);
+					//alert(lon);
+					//alert(name);
 					if ($scope.lat != lat || $scope.lon != lon) {
-						$scope.basel = {lat: lat, lon: lon};
+						$scope.basel = {
+
+							lat: lat,
+							lon: lon,
+							name: name
+						};
+
 						// to be user as markers, objects should have "lat", "lon", and "name" properties
 						$scope.whoiswhere = [ //marker
 							{
-								"name": "You are here!",
+								"name": $scope.basel.name,
 								"lat": $scope.basel.lat,
 								"lon": $scope.basel.lon
 
 							}
+
 						];
 						if (!$scope.$$phase) $scope.$apply("basel");
 					}
-				};
-
-
-
-		$http.get(URL + 'race', $scope).success(function (data) {
-			races = data;
-			angular.forEach(races, function (race) {
-				$scope.objects = race;
-				$scope.Name = race.Name;
-				$scope.Distance = race.Distance;
-				$scope.Level = race.Level;
-				$scope.Date = race.Date;
-				$scope.LocationIniLng = race.LocationIni.Lng;
-				$scope.LocationIniLtd = race.LocationIni.Ltd;
-				goto(race);
-				console.log($scope.LocationIniLng);
-				console.log($scope.LocationIniLtd);
-
-			});
-		});
-
-		function goto(race) {
-			navigator.geolocation.getCurrentPosition(function (position) {
-				//	for (var i = 0; i < 2; i++) {
-				var lat = race.LocationIni.Lng;
-				var lon = race.LocationIni.Ltd;
-				var name = race.Name;
-				gotolocation2(lat, lon, name);
-				$scope.$apply();
-				//}
-			}, function (e) {
-				console.log("Error retrieving position " + e.code + " " + e.message)
-			});
-		function gotolocation2(lat, lon, name) {
-			//alert (lat);
-			//alert(lon);
-			//alert(name);
-			if ($scope.lat != lat || $scope.lon != lon) {
-				$scope.basel = {
-
-					lat: lat,
-					lon: lon,
-					name: name
-				};
-				// to be user as markers, objects should have "lat", "lon", and "name" properties
-				$scope.whoiswhere = [ //marker
-					{
-						"name": $scope.basel.name,
-						"lat": $scope.basel.lat,
-						"lon": $scope.basel.lon
-					}
-				];
-				if (!$scope.$$phase) $scope.$apply("basel");
+				}
 			}
-		}
-	}
-
-				//Obtener carreras html
-				$http.get(URL + 'race').success(function (data) {
-					$scope.races = data;
-					console.log(data);
-				})
-					.error(function (data) {
-						console.log('Error: ' + data);
-					});
+			//Obtener carreras html
+			$http.get(URL + 'race').success(function (data) {
+				$scope.races = data;
+				console.log(data);
+			})
+				.error(function (data) {
+					console.log('Error: ' + data);
+				});
+		//});
 
 	}
+
 ]);
 
 /********************************************************************************
-// formats a number as a latitude (e.g. 40.46... => "40°27'44"N")
-MapApp.filter('lat', function () {
+ // formats a number as a latitude (e.g. 40.46... => "40°27'44"N")
+ MapApp.filter('lat', function () {
     return function (input, decimals) {
         if (!decimals) decimals = 0;
         input = input * 1;
@@ -607,9 +691,8 @@ MapApp.filter('lat', function () {
         return deg + "°" + min + "'" + sec + '"' + ns;
     }
 });
-
-// formats a number as a longitude (e.g. -80.02... => "80°1'24"W")
-MapApp.filter('lon', function () {
+ // formats a number as a longitude (e.g. -80.02... => "80°1'24"W")
+ MapApp.filter('lon', function () {
     return function (input, decimals) {
         if (!decimals) decimals = 0;
         input = input * 1;
@@ -621,59 +704,58 @@ MapApp.filter('lon', function () {
         return deg + "°" + min + "'" + sec + '"' + ew;
     }
 });
-
-****************************************************************************/
+ ****************************************************************************/
 
 /**
  * Handle Google Maps API V3+
  */
 // - Documentation: https://developers.google.com/maps/documentation/
 MapApp.directive("appMap", function ($window) {
-    return {
-        restrict: "E",
-        replace: true,
-        template: "<div></div>",
-        scope: {
-            center: "=",        // Center point on the map (e.g. <code>{ latitude: 10, longitude: 10 }</code>).
-            markers: "=",       // Array of map markers (e.g. <code>[{ lat: 10, lon: 10, name: "hello" }]</code>).
-            width: "@",         // Map width in pixels.
-            height: "@",        // Map height in pixels.
-            zoom: "@",          // Zoom level (one is totally zoomed out, 25 is very much zoomed in).
-            mapTypeId: "@",     // Type of tile to show on the map (roadmap, satellite, hybrid, terrain).
-            panControl: "@",    // Whether to show a pan control on the map.
-            zoomControl: "@",   // Whether to show a zoom control on the map.
-            scaleControl: "@"   // Whether to show scale control on the map.
-        },
-        link: function (scope, element, attrs) {
-            var toResize, toCenter;
-            var map;
-            var infowindow;
-            var currentMarkers;
-   	        var callbackName = 'InitMapCb';
+	return {
+		restrict: "E",
+		replace: true,
+		template: "<div></div>",
+		scope: {
+			center: "=",        // Center point on the map (e.g. <code>{ latitude: 10, longitude: 10 }</code>).
+			markers: "=",       // Array of map markers (e.g. <code>[{ lat: 10, lon: 10, name: "hello" }]</code>).
+			width: "@",         // Map width in pixels.
+			height: "@",        // Map height in pixels.
+			zoom: "@",          // Zoom level (one is totally zoomed out, 25 is very much zoomed in).
+			mapTypeId: "@",     // Type of tile to show on the map (roadmap, satellite, hybrid, terrain).
+			panControl: "@",    // Whether to show a pan control on the map.
+			zoomControl: "@",   // Whether to show a zoom control on the map.
+			scaleControl: "@"   // Whether to show scale control on the map.
+		},
+		link: function (scope, element, attrs) {
+			var toResize, toCenter;
+			var map;
+			var infowindow;
+			var currentMarkers;
+			var callbackName = 'InitMapCb';
 
 			//Obtener la posicion y centrar
-          navigator.geolocation.getCurrentPosition(function(pos) {
-              map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-            }, function(error) {
-              alert('Unable to get location: ' + error.message);
-            });
+			navigator.geolocation.getCurrentPosition(function(pos) {
+				map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+			}, function(error) {
+				alert('Unable to get location: ' + error.message);
+			});
 
-   			// callback when google maps is loaded
+			// callback when google maps is loaded
 			$window[callbackName] = function() {
 				console.log("map: init callback");
 				createMap();
 				updateMarkers();
-				};
+			};
 
 			if (!$window.google || !$window.google.maps ) {
 				console.log("map: not available - load now gmap js");
 				loadGMaps();
-				}
+			}
 			else
-				{
+			{
 				console.log("map: IS available - create only map now");
 				createMap();
-				}
+			}
 			function loadGMaps() {
 				console.log("map: start loading js gmaps");
 				var script = $window.document.createElement('script');
@@ -681,12 +763,12 @@ MapApp.directive("appMap", function ($window) {
 				script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBhqvXyPtM34PhEzbv90sLahqSwQa1cH1A&sensor=true&callback=InitMapCb';
 				//clave google para luego: AIzaSyBhqvXyPtM34PhEzbv90sLahqSwQa1cH1A
 				$window.document.body.appendChild(script);
-				}
+			}
 
 			function createMap() {
 				console.log("map: create map start");
 				var mapOptions = {
-					zoom: 13,
+					zoom: 17,
 					center: new google.maps.LatLng(47.55, 7.59),
 					mapTypeId: google.maps.MapTypeId.ROADMAP,
 					panControl: true,
@@ -696,26 +778,29 @@ MapApp.directive("appMap", function ($window) {
 					streetViewControl: true,
 					navigationControl: true,
 					disableDefaultUI: true,
-					overviewMapControl: true
-					};
+					overviewMapControl: true,
+					addressControlOptions: {
+						position: google.maps.ControlPosition.BOTTOM_CENTER
+					}
+				};
 				if (!(map instanceof google.maps.Map)) {
 					console.log("map: create map now as not already available ");
 					map = new google.maps.Map(element[0], mapOptions);
-          // EDIT Added this and it works on android now
-          // Stop the side bar from dragging when mousedown/tapdown on the map
-          google.maps.event.addDomListener(element[0], 'mousedown', function(e) {
-            e.preventDefault();
-            return false;
-            });
-					infowindow = new google.maps.InfoWindow(); 
-					}
+					// EDIT Added this and it works on android now
+					// Stop the side bar from dragging when mousedown/tapdown on the map
+					google.maps.event.addDomListener(element[0], 'mousedown', function(e) {
+						e.preventDefault();
+						return false;
+					});
+					infowindow = new google.maps.InfoWindow();
 				}
+			}
 
 			scope.$watch('markers', function() {
 				updateMarkers();
-				});
+			});
 
-			// Info window trigger function 
+			// Info window trigger function
 			function onItemClick(pin, label, datum, url, member) {
 				// Create content
 				var contentString = label + "<br />" + "lat: " + pin.position.A + "<br />" + "lon: " + pin.position.F ;
@@ -731,18 +816,18 @@ MapApp.directive("appMap", function ($window) {
 				google.maps.event.addListener(infowindow, 'closeclick', function() {
 					//console.log("map: info windows close listener triggered ");
 					infowindow.close();
-					});
-				} 
+				});
+			}
 
 			function markerCb(marker, member, location) {
-			    return function() {
+				return function() {
 					//console.log("map: marker listener for " + member.name);
 					var href="https://maps.apple.com/?q="+member.lat+","+member.lon;
 					map.setCenter(location);
 					onItemClick(marker, member.name, member.date, href);
 					//console.log(member);
-					};
-				}
+				};
+			}
 
 			// update map markers to match scope marker collection
 			function updateMarkers() {
@@ -761,22 +846,22 @@ MapApp.directive("appMap", function ($window) {
 							title: m.name,
 							//animation: google.maps.Animation.DROP,
 							//animation: google.maps.Animation.BOUNCE,
-							icon: 'img/marker.png'
+							icon: 'http://putopoetayonqui.files.wordpress.com/2010/03/icono-running-mini-36x361.png'
 						});
 						//console.log("map: make marker for " + m.name);
 						google.maps.event.addListener(mm, 'click', markerCb(mm, m, loc));
 						currentMarkers.push(mm);
-						}
 					}
 				}
+			}
 
 			// convert current location to Google maps location
 			function getLocation(loc) {
 				if (loc == null) return new google.maps.LatLng(40, -73);
 				if (angular.isString(loc)) loc = scope.$eval(loc);
 				return new google.maps.LatLng(loc.lat, loc.lon);
-				}
+			}
 
-			} // end of link:
-		}; // end of return
+		} // end of link:
+	}; // end of return
 });

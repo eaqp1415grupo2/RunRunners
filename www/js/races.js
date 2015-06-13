3790156e7dbf0b481f29994ea074e6ee309fc935
AlbertSantamaria <santamaria.lara@gmail.com>
2015-06-13 07:15:38 +0200

    Improved Races, Modals & Checkings

--- www/js/races.js
+++ www/js/races.js
@@ -1,11 +1,23 @@
 angular.module('races.controller', [])
 
-.controller('RacesCtrl', function($scope, $http, $stateParams, $ionicLoading, $log) {
+.controller('RacesCtrl', function($scope, $ionicModal, $http, $window, $stateParams, $ionicLoading, $log) {
     //var URL='https://10.189.25.180:3030/';
     var URL='https://localhost:3030/';
     $scope.ownraces = [];
     $scope.otherraces = [];
+    $scope.rid = [];
+    $scope.user = [];
 
+	 $scope.getUser = function () {
+        $http.get(URL + 'user/' + $window.localStorage['token'])
+            .success(function (data) {
+                $scope.user = data;
+            })
+            .error(function (data) {
+                console.log('Error:' + data);
+            });
+    }; 
+    
     $scope.loadRaces=function () {
         //Obtener carreras propias
         $http.get(URL+'race/user/'+window.localStorage.token).success(function(data) {
@@ -56,6 +68,48 @@ angular.module('races.controller', [])
         .error(function (data) {
             console.log('Error:' + data);
         });
+    };
+
+  	$ionicModal.fromTemplateUrl('deleteRace.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.openDeleteRaceModal = function (rid,admin) {
+    	  $scope.rid=rid;
+    	  $scope.getUser();
+    	  console.log("rid "+ rid +" Admin "+admin + " Username "+$scope.user.Username)
+    	  if (admin==$scope.user.Username) {
+           $scope.modal.show(); 	  
+    	  }else {
+			$window.alert("No puedes elimirar un grupo si no eres su Administrador!")		  
+		  }
+    };
+    
+    $scope.deleteRace = function () {
+    	
+		$http({
+			method: 'DELETE',
+			url: URL+'race/'+$scope.rid,
+			data: {_id:window.localStorage.token},
+			headers: {'Content-Type': 'application/json'}
+		}).success(function (data) {
+				//window.location.href = '#/map/races/';
+				console.log(data);
+				$scope.loadRaces();
+				$scope.modal.hide();
+			})
+			.error(function (data) {
+				$window.alert(data);
+				console.log('Error:' + data);
+			});
+	}; 
+
+    $scope.closeModal = function () {
+        $scope.modal.hide();
+        //$scope.modal2.hide();
     };
 
 });

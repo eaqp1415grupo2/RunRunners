dda3058dc6019b31816bd9806a3081b6f8185971
AlbertSantamaria <santamaria.lara@gmail.com>
2015-06-13 05:04:50 +0200

    Modal for New Group & Delete Group

--- www/js/groups.js
+++ www/js/groups.js
@@ -1,8 +1,9 @@
 angular.module('groups.controller', [])
 
-.controller('GroupsCtrl', function ($scope,$http, $ionicLoading, $log, GroupMessageService) {
+.controller('GroupsCtrl', function ($scope, $http, $ionicModal, $window, $ionicLoading, $log) {
     $scope.owngroups = [];
     $scope.othergroups = [];
+    $scope.newgroup = [];
 
     $scope.loadGroups=function () {
 
@@ -59,11 +60,73 @@ angular.module('groups.controller', [])
                 console.log('Error:' + data);
             });
     };
+    
+   //NewGroupModal
+	$ionicModal.fromTemplateUrl('NewGroupModal.html', {
+        scope: $scope,
+        animation: 'slide-in-up'
+    }).then(function (modal) {
+        $scope.modal = modal;
+    });
+
+    $scope.openNewGroupModal = function () {
+        $scope.modal.show();
+    };
+
+	$scope.postNewGroup = function () {
+		console.log("answer: "+$scope.newgroup);
+		if(($scope.newgroup.Name==null)||($scope.newgroup.Name=="")){
+		//console.log("Mensaje vacio no se postea");
+		$window.alert("Debes escribir el nombre del grupo!");
+		}else{
+		$http({
+			method: 'POST',
+			url: URL+'groups',
+			data: {
+					_id:window.localStorage.token,
+					Info:$scope.newgroup.Info,
+					Name:$scope.newgroup.Name,
+					Level:$scope.newgroup.Level,
+					Location:$scope.newgroup.Location
+					},
+			headers: {'Content-Type': 'application/json'}
+		}).success(function (data) {
+				console.log(data);
+				
+				$scope.loadGroups();
+				$scope.newgroup.Info="";
+				$scope.newgroup.Name="";
+				$scope.newgroup.Level="";
+				$scope.newgroup.Location="";
+				$scope.modal.hide();
+			})
+			.error(function (data) {
+				console.log('Error:' + data);
+			});
+	
+	}
+	};
+
+
+    $scope.closeNewGroupModal = function () {
+        $scope.modal.hide();
+    };
+    
+    $scope.deleteGroup = function (gid) {
+		$http({
+			method: 'DELETE',
+			url: URL+'groups/'+gid,
+			data: {_id:window.localStorage.token},
+			headers: {'Content-Type': 'application/json'}
+		}).success(function (data) {
+				//window.location.href = '#/map/races/';
+				console.log(data);
+				$scope.loadGroups();
+			})
+			.error(function (data) {
+				$window.alert(data);
+				console.log('Error:' + data);
+			});
+	}; 
+    
 })
-.service('GroupMessageService',function ($http, $log,$stateParams) {
-    this.loadGroupMessages = function() {
-        $http.get(URL+'message/parent/'+$stateParams.groupId).success(function (data) {
-            $scope.messages=data;
-        });
-    }
-});

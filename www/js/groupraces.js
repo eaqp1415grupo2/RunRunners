5bb30dae52e825244fb6bcdb5d1090699a59a980
AlbertSantamaria <santamaria.lara@gmail.com>
2015-06-14 21:03:27 +0200

    Fix AddRace to Group web&Android

--- www/js/groupraces.js
+++ www/js/groupraces.js
@@ -8,6 +8,7 @@ angular.module('groupraces.controller', [])
     $scope.otherraces = [];
     $scope.rid = [];
     $scope.user = [];
+    $scope.Admin = [];
 
 	 $scope.getUser = function () {
         $http.get(URL + 'user/' + $window.localStorage['token'])
@@ -19,16 +20,43 @@ angular.module('groupraces.controller', [])
             });
     }; 
     
-    $scope.addRaceToGroup = function (id) {
+    $scope.getGroupAdmin = function () {
+        $http.get(URL + 'groups/' + $stateParams.id)
+            .success(function (data) {
+                $scope.Admin = data;
+            })
+            .error(function (data) {
+                console.log('Error:' + data);
+            });
+    };
+    
+   $scope.initListGroupRace = function () {
+
+		$scope.getUser();
+		$scope.getGroupAdmin();
+			console.log($scope.user+' '+$scope.Admin);
+		if ($scope.user=$scope.Admin) {
+		
+		 $scope.loadGroupRaces();
+		 $scope.loadNoGroupRaces();
+
+		}else {
+		
+			$scope.loadGroupRaces();}
+  	 };
+
+    $scope.addRace = function (id) {
         $http({
             method: 'POST',
-            url: URL+'group/'+$stateParams.id+'/race',
+            url: URL+'groups/'+$stateParams.id+'/race',
             data: {_id:window.localStorage.token,
             		raceid:id},
             headers: {'Content-Type': 'application/json'}
         }).success(function (data) {
-            $scope.loadRaces();
-            console.log(data);
+				$scope.groupraces="";
+				$scope.otherraces="";
+				
+				$scope.initListGroupRace();
 
         })
         .error(function (data) {
@@ -38,10 +66,9 @@ angular.module('groupraces.controller', [])
     
     $scope.loadGroupRaces=function () {
         //Obtener carreras propias
-        $http.get(URL+'groups/'+$stateParams.id+'/race').success(function(data) {
+        $http.get(URL+'race/group/'+$stateParams.id).success(function(data) {
             $scope.groupraces = data;
-            console.log('Group Races:'+$scope.groupraces);
-            console.log('Group Races:'+data.length);
+            console.log('own Races:'+$scope.groupraces);
         })
         .error(function(data) {
             console.log('Error: ' + data);
@@ -51,7 +78,7 @@ angular.module('groupraces.controller', [])
     
     $scope.loadNoGroupRaces=function () {
         //Obtener otras carreras
-        $http.get(URL+'race/').success(function(data) {
+        $http.get(URL+'race/no/group/'+$stateParams.id).success(function(data) {
             $scope.otherraces = data;
             console.log('other Races:'+$scope.otherraces);
         })
@@ -70,10 +97,11 @@ angular.module('groupraces.controller', [])
 			},
 			headers: {'Content-Type': 'application/json'}
 		}).success(function (data) {
-				//window.location.href = '#/map/races/';
-				console.log('Eliminado OK'+data);
-				$scope.loadGroupRaces();
-				$scope.loadNoGroupRaces();
+				console.log('Eliminado '+data);
+				$scope.groupraces="";
+				$scope.otherraces="";
+				
+				$scope.initListGroupRace();
 			})
 			.error(function (data) {
 				$window.alert(data);
